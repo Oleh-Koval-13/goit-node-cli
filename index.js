abe47bb166f099cd727ab5e97a0493cff7b551f4
Oleh-Koval-13 <solder363191999@gmail.com>
2024-05-12 15:47:48 +0300

    update

---
+++ index.js
@@ -0,0 +1,43 @@
+import { program } from "commander";
+import Contacts from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+    const contacts = await Contacts.listContacts();
+    console.table(contacts);
+    break;
+      
+    case "get":
+    const contact = await Contacts.getContactById(id);
+    console.log(contact);     
+    break;
+    
+    case "add":
+    const newContact = await Contacts.addContact(name, email, phone);
+    console.log(newContact);
+    break;
+    
+    case "remove":
+    const deletedContact = await Contacts.removeContact(id);
+    console.log(deletedContact);
+    break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
